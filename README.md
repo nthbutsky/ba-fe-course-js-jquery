65782339b3ce97a88c060870211f7d8a090db171
Denis Butsky <nthbutsky@gmail.com>
2021-10-12 17:28:26 +0300

    add task 1 for lesson 32

---
+++ README.md
@@ -0,0 +1,5 @@
+# ba-fe-course-js
+
+Beetroot Academy Front-End Course Homework Part 2 JavaScript
+
+🚧 [DEMO](https://nthbutsky.github.io/ba-fe-course-js/) 🚧
---
+++ src/files.js
@@ -0,0 +1,4 @@
+import cart from './img/cart.svg';
+import rating from './img/rating.svg';
+import img from './img/img.png';
+import info from './img/info.svg';
